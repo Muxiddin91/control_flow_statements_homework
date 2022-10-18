ce4e604a606d4b8f371f174119d8e8f84c5d07ab
muxiddin <muxiddintemirov@gmail.com>
2022-10-18 16:33:41 +0500

    09 bajarildi

--- if09.py
+++ if09.py
@@ -9,4 +9,9 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    b=a%10*10+a//10
+    if a>=b:
+        return "true"
+    else:
+        return "false"
+print(main(36))
